b10c6f8ad3763b19d2bee2c633c372ac756af6c1
youhyeoneee <khukyh2018@khu.ac.kr>
2024-02-18 19:41:40 +0900

    feat: Campaign 스키마 설계

---
+++ model/Campaign.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const campaignSchema = new mongoose.Schema({
+    campaignId: { type: String, required: true },
+    categoryName: { type: String, required: true },
+    title: { type: String, required: true },
+    totalBackedAmount: { type: Number, required: true },
+    photoUrl: { type: String, required: true },
+    nickName: { type: String, required: true },
+    coreMessage: { type: String, required: true },
+    whenOpen: { type: Date, required: true },
+    achievementRate: { type: Number, required: true },
+});
+
+const Campaign = mongoose.model("Campaign", campaignSchema);
+
+module.exports = Campaign;
