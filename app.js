b07ea6c3e97a09ecdbcce28d21dd2931c07e02d7
youhyeoneee <khukyh2018@khu.ac.kr>
2024-02-18 19:42:10 +0900

    feat: mongoose로 DB 연결

--- app.js
+++ app.js
@@ -1,41 +1,62 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const campaignRouter = require("./routes/campaign");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/api", campaignRouter);
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
 
+dotenv.config();
+
+const username = process.env.USER_NAME;
+const password = process.env.PASSWORD;
+const cluster = process.env.CLUSTER;
+const database = process.env.DB;
+const MONGO_URL = `mongodb+srv://${username}:${password}@${cluster}/${database}`;
+
+mongoose
+    .connect(MONGO_URL, {
+        retryWrites: true,
+        w: "majority",
+    })
+    .then((resp) => {
+        console.log(resp);
+        console.log("SUCCESS CONNECTION");
+    })
+    .catch((err) => console.log(err));
 module.exports = app;
---
+++ routes/campaign.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+const Campaign = require("../model/Campaign.js");
+
+// 조회
+router.get("/campaign", function (req, res, next) {
+    Campaign.find()
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((err) => {
+            return next(err);
+        });
+});
+
+module.exports = router;
